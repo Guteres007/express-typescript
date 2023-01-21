57d6bc84fcb21288b1a0586c09ac96f21cb6f543
Martin Andráši <andrasi.m007@gmail.com>
2023-01-21 15:55:22 +0100

    schema

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,68 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express from 'express'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import * as http from 'http'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+// ...other imports
+import { readFileSync } from 'fs'
+
+// Note: this uses a path relative to the project's
+// root directory, which is the current working directory
+// if the server is executed using `npm run`.
+const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' })
 
 const app = express()
-app.use(express.json())
 const PORT = 3000
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    next()
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app)
 
-    res.status(200).json({
-        message: 'Running Node with Express and Typescript'
-    })
+interface MyContext {
+    token?: string
+}
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin'
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster'
+    }
+]
+const resolvers = {
+    Query: {
+        books: () => books
+    }
+}
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+
+const server = new ApolloServer<MyContext>({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+})
+
+server.start().then((value) => {
+    app.use(
+        '/',
+        cors<cors.CorsRequest>(),
+        bodyParser.json(),
+        // expressMiddleware accepts the same arguments:
+        // an Apollo Server instance and optional configuration options
+        expressMiddleware(server, {
+            context: async ({ req }) => ({ token: req.headers.token })
+        })
+    )
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}.`)
+    console.log('Web běží na tomto portu: ' + PORT)
 })
